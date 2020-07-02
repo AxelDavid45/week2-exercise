9f83a6b2525ffce7b786ba91b8e34d013eb039d5
Axel Espinosa <axeldavid45@gmail.com>
2020-07-01 20:17:22 -0500

    Stop propagation in image

--- js/index.js
+++ js/index.js
@@ -4,5 +4,15 @@ const date = new Date().getFullYear();
 //Insert the year
 footerYear.innerText = date.toString();
 
-//Event
+//Nav listener
 const nav = new NavigationImplementation();
+
+//Event propagation
+const article = document.getElementById('composed-father');
+article.addEventListener('click', () => alert('Propagated'));
+
+const image = document.getElementById('composed-event');
+image.addEventListener('click', (evt) => {
+    evt.stopPropagation();
+    alert('I\'m not propagating me');
+})
